8d1961ffd4a338ed7f87c187b17b593852a3f22a
cesarvecchio <ozaro.cesar@gmail.com>
2024-02-05 19:42:12 -0300

    Subindo projeto base

---
+++ src/main/java/br/com/restauranteadjt/RestauranteAdjtApplication.java
@@ -0,0 +1,13 @@
+package br.com.restauranteadjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteAdjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteAdjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restauranteadjt/RestauranteAdjtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restauranteadjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteAdjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
