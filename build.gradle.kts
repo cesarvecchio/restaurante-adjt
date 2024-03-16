3fadfce50e28cb83c7861ab35b18a74404bea975
João Marcos <jmasjesus@protonmail.com>
2024-03-16 16:37:57 -0300

    Adicionando testes integrados RestauranteControllerIT

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,14 +29,16 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.5.2")
+	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.11.0")
 	testImplementation("io.rest-assured:rest-assured:5.3.0") {
 		exclude(group = "org.codehaus.groovy", module = "groovy")
 		exclude(group = "org.codehaus.groovy", module = "groovy-xml")
 	}
+	testImplementation("io.rest-assured:json-schema-validator:5.3.1")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
 	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 	testImplementation("org.assertj:assertj-core:3.24.2")
+	testImplementation("org.apache.commons:commons-compress:1.26.1")
 }
 
 tasks.withType<Test> {
